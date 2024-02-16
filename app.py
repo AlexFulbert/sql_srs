79e4d20df845803c3423f1cd4229d4dbcf5c4668
Alex Fulbert <alexfulbert@MacBook-Pro-de-Alex.local>
2024-02-16 15:23:51 +0100

    add themes selection

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import pandas as pd
 import streamlit as st
 import duckdb
 
+st.write("#SQL SRS - Spaced Repetition System SQL practice")
+option = st.selectbox('What would you like to review', ('Joins', 'GroupBy', 'Windows Functions'), index=None, placeholder="Select option")
+st.write(f"You selected {option}")
+
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
 tab1, _ = st.tabs(['h', ''])
 
 with tab1:
-
     sql_query = st.text_area(label='text')
     try:
         result = duckdb.sql(sql_query).df()
