0ecf6ef3e2d4ed26329a3975d654dd3a98828dba
Alex Fulbert <alexfulbert@MacBook-Pro-de-Alex.local>
2024-02-16 16:37:56 +0100

    add side bar

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ import pandas as pd
 import streamlit as st
 import duckdb
 
-st.write("#SQL SRS - Spaced Repetition System SQL practice")
-option = st.selectbox('What would you like to review', ('Joins', 'GroupBy', 'Windows Functions'), index=None, placeholder="Select option")
-st.write(f"You selected {option}")
+st.write("""
+# SQL SRS
+Spaced Repetition System SQL practice
+""")
+
+with st.sidebar:
+    option = st.selectbox(
+        'What would you like to review', ('Joins', 'GroupBy', 'Windows Functions'
+                                          ), index=None, placeholder="Select option")
+    st.write(f"You selected {option}")
 
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
